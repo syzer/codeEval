4c34ccff9cbfc4db09055a3683c6ed3379ca0f4d
syzer <syzer3@gmail.com>
2014-10-30 19:19:22 +0100

    SQL command

--- private/src/client/app/functional/functional.controller.js
+++ private/src/client/app/functional/functional.controller.js
@@ -1,17 +1,22 @@
 'use strict';
 
 angular.module('jsSparkUiApp')
-    .controller('FunctionalCtrl', function ($scope) {
-        $scope.jsCode = {};
+    .controller('FunctionalCtrl', function ($scope, _) {
 
-        $scope.editorOptions = {
+        $scope.jsCode = $(document).ready(function () {
+            $("#flip").click(function () {
+                $("#panel").slideUp("slow");
+            });
+        });
+
+        $scope.jsOptions = {
             lineWrapping: true,
-//            lineNumbers: true,
-//            readOnly: 'nocursor',
             showCursorWhenSelecting: true,
             tabSize: 4,
             mode: "javascript"
         };
+        $scope.sqlOptions = _.clone($scope.jsOptions);
+        $scope.sqlOptions.mode = "sql";
 
 //        $(document).ready(function () {
 //            $("#flip").click(function () {
@@ -19,17 +24,19 @@ angular.module('jsSparkUiApp')
 //            });
 //        });
 
-        function sql() {
-            return "SELECT Book.title AS Title,\
+        $scope.sqlCode = "SELECT Book.title AS Title,\
                 COUNT(*) AS Authors\
             FROM  Book\
             JOIN  Book_author\
             ON  Book.isbn = Book_author.isbn\
             GROUP BY Book.title;";
+
+        $scope.change = function(item) {
+            console.log(item);
         };
 
         $scope.testMe = function() {
             console.log($scope.jsCode);
-        }
+        };
     });
 
