c20b0cb882d028468dffca31946251182b140dfd
syzer <syzer3@gmail.com>
2014-10-30 20:12:03 +0100

    SQL works

--- private/src/client/app/functional/functional.controller.js
+++ private/src/client/app/functional/functional.controller.js
@@ -3,39 +3,38 @@
 angular.module('jsSparkUiApp')
     .controller('FunctionalCtrl', function ($scope, _) {
 
-        $scope.jsCode = $(document).ready(function () {
-            $("#flip").click(function () {
-                $("#panel").slideUp("slow");
-            });
-        });
-
         $scope.jsOptions = {
             lineWrapping: true,
             showCursorWhenSelecting: true,
             tabSize: 4,
-            mode: "javascript"
+            mode: "javascript",
+            matchBrackets: true
         };
         $scope.sqlOptions = _.clone($scope.jsOptions);
-        $scope.sqlOptions.mode = "sql";
+        $scope.sqlOptions.mode = "text/x-mysql";
 
-//        $(document).ready(function () {
-//            $("#flip").click(function () {
-//                $("#panel").slideUp("slow");
-//            });
-//        });
+        $scope.jsCode = [
+            "$(document).ready(function () {",
+            "    $('#flip').click(function () {",
+            "        $('#panel').slideUp('slow');",
+            "    });",
+            "});"
+        ].join('\n');
 
-        $scope.sqlCode = "SELECT Book.title AS Title,\
-                COUNT(*) AS Authors\
-            FROM  Book\
-            JOIN  Book_author\
-            ON  Book.isbn = Book_author.isbn\
-            GROUP BY Book.title;";
+        $scope.sqlCode = [
+            "SELECT Book.title AS Title,",
+            "COUNT(*) AS Authors",
+            "FROM  Book",
+            "JOIN  Book_author",
+            "ON  Book.isbn = Book_author.isbn",
+            "GROUP BY Book.title;"
+        ].join('\n');
 
-        $scope.change = function(item) {
+        $scope.change = function (item) {
             console.log(item);
         };
 
-        $scope.testMe = function() {
+        $scope.testMe = function () {
             console.log($scope.jsCode);
         };
     });
