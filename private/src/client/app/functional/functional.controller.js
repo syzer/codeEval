f434a15eb5875746773d6d4e6652a375e0157b93
syzer <syzer3@gmail.com>
2014-10-31 10:53:18 +0100

    added logo refactored some code

--- private/src/client/app/functional/functional.controller.js
+++ private/src/client/app/functional/functional.controller.js
@@ -1,41 +1,11 @@
 'use strict';
 
 angular.module('jsSparkUiApp')
-    .controller('FunctionalCtrl', function ($scope, _) {
+    .controller('FunctionalCtrl', function ($scope, _, intro) {
 
-        $scope.jsOptions = {
-            lineWrapping: true,
-            showCursorWhenSelecting: true,
-            tabSize: 4,
-            mode: "javascript",
-            matchBrackets: true
-        };
-        $scope.sqlOptions = _.clone($scope.jsOptions);
-        $scope.sqlOptions.mode = "text/x-mysql";
-
-        $scope.jsCode = [
-            "$(document).ready(function () {",
-            "    $('#flip').click(function () {",
-            "        $('#panel').slideUp('slow');",
-            "    });",
-            "});"
-        ].join('\n');
-
-        $scope.sqlCode = [
-            "SELECT Book.title AS Title,",
-            "COUNT(*) AS Authors",
-            "FROM  Book",
-            "JOIN  Book_author",
-            "ON  Book.isbn = Book_author.isbn",
-            "GROUP BY Book.title;"
-        ].join('\n');
-
-        $scope.change = function (item) {
-            console.log(item);
-        };
-
-        $scope.testMe = function () {
-            console.log($scope.jsCode);
-        };
+        $scope.js = intro.js;
+        $scope.sql = intro.sql;
+        $scope.xml = intro.xml;
+        $scope.logo = intro.logo;
     });
 
--- private/src/client/app/functional/functional.js
+++ private/src/client/app/functional/functional.js
@@ -15,5 +15,88 @@ angular.module('jsSparkUiApp')
         return function booleanFilter(input) {
             return input ? '\u2713' : '\u2718';
         };
+    })
+    .factory('intro', function (_) {
+        var jsOptions = {
+            lineWrapping: true,
+            showCursorWhenSelecting: true,
+            tabSize: 4,
+            mode: "javascript",
+            matchBrackets: true
+        };
+
+        var sqlOptions = _.clone(jsOptions);
+        sqlOptions.mode = "text/x-mysql";
+
+        var xmlOptions = _.clone(jsOptions);
+        xmlOptions.mode = "text/x-mysql";
+
+        var logoOptions = _.clone(jsOptions);
+        logoOptions.mode = "text/x-verilog";
+
+        var jsCode = [
+            "$(document).ready(function () {",
+            "    $('#flip').click(function () {",
+            "        $('#panel').slideUp('slow');",
+            "    });",
+            "});"
+        ].join('\n');
+
+        var sqlCode = [
+            "SELECT Book.title AS Title,",
+            "COUNT(*) AS Authors",
+            "FROM  Book",
+            "JOIN  Book_author",
+            "ON  Book.isbn = Book_author.isbn",
+            "GROUP BY Book.title;"
+        ].join('\n');
+
+        var xmlCode = [
+            "<note>",
+            "	<to>Tove</to>",
+            "	<from>Jani</from>",
+            "	<heading>Reminder</heading>",
+            "	<body>Dont forget me this weekend!</body>",
+            "</note>"
+        ].join('\n');
+
+        var logoCode = [
+            "#logo",
+            "to reverse :stuff",
+            "ifelse equal? count :stuff 1",
+            "[output first :stuff]",
+            "[output sentence reverse butfirst :stuff first :stuff]",
+            "end",
+            "",
+            "print reverse [apples and pears]",
+            "pears and apples"
+        ].join('\n');
+
+        return {
+            js: {
+                caption: 'Being alive after 2006 and do WebDevelopment:',
+                code: jsCode,
+                opts: jsOptions
+            },
+            sql: {
+                caption: 'Used a DB:',
+                code: sqlCode,
+                opts: sqlOptions
+            },
+            xml: {
+                caption: 'Used XML:',
+                code: xmlCode,
+                opts: xmlOptions
+            },
+            logo: {
+                caption: 'Was forced @school to do logo:',
+                code: logoCode,
+                opts: logoOptions
+            }
+        }
+
+    })
+    .factory('task1', function(){
+
     });
 
